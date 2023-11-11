47aa3c582981adfda99eaae4fbf0210cb44f77bd
chimpadigialr <sprados@chimpancedigital.com.ar>
2023-11-11 13:17:17 -0300

    fix property detail

--- src/app/(property)/(single-style)/single-v1/[id]/page.js
+++ src/app/(property)/(single-style)/single-v1/[id]/page.js
@@ -71,7 +71,7 @@ const SingleV1 = ({params}) => {
 
                 <h4 className="title fz17 mb30 mt50">Property Details</h4>
                 <div className="row">
-                  <PropertyDetails111111111111111111111111111111111111111111111 />
+                  <PropertyDetails/>
                 </div>
               </div>
               {/* End .ps-widget */}
